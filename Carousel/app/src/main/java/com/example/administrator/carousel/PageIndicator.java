250eb319985635b80ed066912437a642162b8b56
denha995 <denha995@student.liu.se>
2016-12-13 16:10:52 +0100

    Added custom view for PageIndicator

--- Carousel/app/src/main/java/com/example/administrator/carousel/PageIndicator.java
+++ Carousel/app/src/main/java/com/example/administrator/carousel/PageIndicator.java
@@ -1,7 +1,14 @@
 package com.example.administrator.carousel;
 
+
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Region;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -11,17 +18,42 @@ import android.view.View;
 
 public class PageIndicator extends View {
 
+    private Paint paint;
+    private Rect boundsRect;
+    private int width;
+    private int height;
+
     public PageIndicator(Context context) {
         super(context);
+
+        // create the Paint and set its color
+        init();
+    }
+
+    public PageIndicator(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        init();
+    }
+
+    public PageIndicator(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+        canvas.drawColor(Color.BLUE);
+        Log.i("PageIndicator", "width: " + getWidth() + " Height: " + getHeight());
+        canvas.clipRect(0, 0, getWidth(), getHeight(), Region.Op.REPLACE);
+        canvas.drawCircle(getWidth()/2, getHeight()/2, 20, paint);
+
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    private void init(){
+        paint = new Paint();
+        paint.setColor(Color.GRAY);
+        boundsRect = new Rect();
     }
+
 }
