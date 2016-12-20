7b4108a6abff491c3502fa51640f167c29af0aa1
torsten92 <torgu529@student.liu.se>
2016-12-20 14:14:53 +0100

    changed account registration from activity to popupmanager

--- AccountRegistration/app/src/main/java/com/example/administrator/accountregistration/MainActivity.java
+++ AccountRegistration/app/src/main/java/com/example/administrator/accountregistration/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,9 +26,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void EnterMyPages() {
-        Intent intent = new Intent(this, AccountRegistrationActivity.class);
-        intent.putExtra("REQUEST", "CHECKLOGGEDIN");
-        startActivity(intent);
+        if(AccountRegistration.IsLoggedIn()) {
+            Intent intent = new Intent(this, MyPagesActivity.class);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(this, "You must log in first!", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
