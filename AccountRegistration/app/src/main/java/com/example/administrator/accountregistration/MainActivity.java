e5bb419525ade8bf6e0b2689d97d2e60e59c4471
torsten92 <torgu529@student.liu.se>
2016-12-20 12:53:35 +0100

    Added base structure for account registration

--- AccountRegistration/app/src/main/java/com/example/administrator/accountregistration/MainActivity.java
+++ AccountRegistration/app/src/main/java/com/example/administrator/accountregistration/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.administrator.accountregistration;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button myPages = (Button) findViewById(R.id.buttonMyPages);
+
+        myPages.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            EnterMyPages();
+            }
+        });
+    }
+
+    private void EnterMyPages() {
+        Intent intent = new Intent(this, MyPagesActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
